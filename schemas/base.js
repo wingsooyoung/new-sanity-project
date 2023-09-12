5ef244497761230aff984de3f1e49811f06dd356
wingsooyoung <hsm_rox_16@hotmail.com>
2023-09-11 23:37:10 -0400

    Update base.js

--- schemas/base.js
+++ schemas/base.js
@@ -1,16 +1,22 @@
 // schemas/base.js
 export default {
-  title: 'Base',
-  name: 'base',
-  type: 'image',
-  options: {
-
-  },
+  title: 'Base', //human readable label
+  name: 'base', //the field name, this will be the key to access within the data
+  type: 'document', //name of a valid schema type - DOCUMENT is what every other type is stored inside of!
   fields: [
     {
-      name: 'caption',
-      type: 'string',
-      title: 'Caption',
+      title: 'Postcard Base',
+      name: 'postcardBase',
+      type: 'image',
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+        }
+      ]
     }
   ]
 }
+
+
