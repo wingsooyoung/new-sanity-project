297b1dc93c0a657f8618c576bb903b3a8100af2c
wingsooyoung <hsm_rox_16@hotmail.com>
2023-09-11 23:32:13 -0400

    Create base.js

---
+++ schemas/base.js
@@ -0,0 +1,16 @@
+// schemas/base.js
+export default {
+  title: 'Base',
+  name: 'base',
+  type: 'image',
+  options: {
+
+  },
+  fields: [
+    {
+      name: 'caption',
+      type: 'string',
+      title: 'Caption',
+    }
+  ]
+}
