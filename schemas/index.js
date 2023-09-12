bb14f3c70640edc60c66d6ae9258323ca5c040fb
wingsooyoung <hsm_rox_16@hotmail.com>
2023-09-11 23:32:46 -0400

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,7 @@
 import pet from './pet'
 import first from './first'
 import postcard from './postcard-1'
+import base from './base'
 
-export const schemaTypes = [pet, first, postcard]
+
+export const schemaTypes = [pet, first, postcard, base]
