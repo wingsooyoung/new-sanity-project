cf03022b4ecb3868d9a2514ec122941ad12901d1
wingsooyoung <hsm_rox_16@hotmail.com>
2023-11-05 17:18:54 -0500

    Update index.js
    Add newbase (base-2) schema type

--- schemas/index.js
+++ schemas/index.js
@@ -3,6 +3,7 @@ import pet from './pet'
 import first from './first'
 import postcard from './postcard-1'
 import base from './base'
+import newbase from './base-2'
 
 
-export const schemaTypes = [pet, first, postcard, base]
+export const schemaTypes = [pet, first, postcard, base, newbase]
